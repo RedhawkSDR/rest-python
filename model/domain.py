8b98af30a93ba25cd88d1435526214b0e02ad569
Rob Cannon <rob.cannon@axiosengineering.com>
2014-08-18 15:44:01 -0400

    Premiminary properties implementation

--- model/domain.py
+++ model/domain.py
@@ -1,12 +1,15 @@
+from omniORB import CORBA
 from ossie.utils import redhawk
 from ossie.utils.redhawk.channels import ODMListener
-
 __author__ = 'rpcanno'
 
 
 def scan_domains():
     return redhawk.scan()
 
+
+def _my_dir(obj):
+    return [m for m in dir(obj) if not m.startswith('__')]
 
 class Domain:
     domMgr_ptr = None
@@ -35,10 +38,77 @@ class Domain:
         self.domMgr_ptr = redhawk.attach(self.name)
         self._connect_odm_listener()
 
+    def _any_simple(self, any):
+        return {'type': 'simple', 'value': any._v}
+
+    def _any_struct(self, any):
+        ret = {'type': 'struct'}
+        value = {}
+        for a in any._v:
+            #ret['value'].append(self._prop(a))
+            value[a.id] = a.value._v
+        ret['value'] = value
+        return ret
+
+    def _any_seq(self, any):
+        ret = {'type': 'seq', 'value': []}
+        for a in any._v:
+            ret['value'].append(self._any(a))
+        return ret
+
+    def _any(self, any):
+        typeName = str(any._t)
+
+        if 'CORBA.TypeCode("IDL:CF/Properties:1.0")' == typeName:
+            return self._any_struct(any)
+        elif 'CORBA.TypeCode("IDL:omg.org/CORBA/AnySeq:1.0")' == typeName:
+            return self._any_seq(any)
+        else:
+            return self._any_simple(any)
+
+    def _prop(self, property):
+        ret = self._any(property.value)
+        ret['id'] = property.id
+        return ret
+
     def _props(self, properties):
         prop_dict = []
         for prop in properties:
-            prop_dict.append({'id': prop.id, "value": prop.value.value()})
+            prop_dict.append(self._prop(prop))
+
+        return prop_dict
+
+    def _propSet(self, properties):
+        prop_dict = []
+        for prop in properties:
+            if prop.type != 'struct' and prop.type != 'structSeq':
+                if isinstance(prop.queryValue(), list):
+                    prop_type = "simpleSeq"
+                else:
+                    prop_type = 'simple'
+            else:
+                prop_type = prop.type
+
+            prop_json = {
+                'id': prop.id,
+                'name': prop.clean_name,
+                'value': prop.queryValue(),
+                'scaType': prop_type,
+                'mode': prop.mode,
+                'kinds': prop.kinds
+            }
+
+            if prop_type == 'simple':
+                prop_json['type'] = type(prop.queryValue()).__name__
+                if prop_json['type'] == 'str':
+                    prop_json['type'] = 'string'
+                elif prop_json['type'] == 'bool':
+                    prop_json['type'] = 'boolean'
+
+            if '_enums' in dir(prop) and prop._enums:
+                prop_json['enumerations'] = prop._enums
+
+            prop_dict.append(prop_json)
         return prop_dict
 
     def _ports(self, ports):
@@ -52,8 +122,8 @@ class Domain:
         return port_dict
 
     def properties(self):
-        props = self.domMgr_ptr.query([])
-        return self._props(props)
+        props = self._props(self.domMgr_ptr.query([]))  # TODO: self._propSet(self.domMgr_ptr._properties)
+        return props
 
     def info(self):
         if self.domMgr_ptr:
@@ -81,12 +151,13 @@ class Domain:
                 comp_dict = []
                 for comp in app.comps:
                     comp_dict.append({"name": comp.name, "id": comp._id})
+                prop_dict = self._propSet(app._properties)  # self._props(app.query([]))
                 return {
                     'id': app._get_identifier(),
                     'name': app.name,
                     'components': comp_dict,
                     'ports': self._ports(app.ports),
-                    'properties': self._props(app.query([]))
+                    'properties': prop_dict
                 }
         return None
 
@@ -95,15 +166,13 @@ class Domain:
             if app._get_identifier() == app_id:
                 for comp in app.comps:
                     if comp._id == comp_id:
-                        prop_dict = []
-                        for prop in comp._properties:
-                            prop_dict.append({"name": prop.clean_name, "value": str(prop.queryValue())})
+                        prop_dict = self._propSet(comp._properties)  # self._props(comp.query([]))
 
                         return {
                             'name': comp.name,
                             'id': comp._id,
                             'ports': self._ports(comp.ports),
-                            'properties': self._props(comp.query([]))
+                            'properties': prop_dict
                         }
         return None
 
@@ -163,10 +232,12 @@ class Domain:
                 svc_dict = []
                 for svc in devMgr.services:
                     svc_dict.append({"name": svc.name, "id": svc._id})
+
+                prop_dict = self._props(devMgr.query([]))  # TODO: self._propSet(devMgr._properties)
                 return {
                     'name': devMgr.name,
                     'id': devMgr.id,
-                    'properties': self._props(devMgr.query([])),
+                    'properties': prop_dict,
                     'devices': dev_dict,
                     'services': svc_dict
                 }
@@ -186,15 +257,14 @@ class Domain:
             if devMgr.id == dev_mgr_id:
                 for dev in devMgr.devs:
                     if dev._id == dev_id:
-                        prop_dict = []
-                        for prop in dev._properties:
-                            prop_dict.append({"name": prop.clean_name, "value": prop.queryValue()})
+                        prop_dict = self._propSet(dev._properties)
 
                         return {
                             'name': dev.name,
                             'id': dev._id,
                             'started': dev._get_started(),
                             'ports': self._ports(dev.ports),
+                            # 'properties': self._props(dev.query([])),
                             'properties': prop_dict
                         }
         return None
