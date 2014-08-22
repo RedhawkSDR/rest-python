05a7563d8e29a175c413e268aff27877cc8e1aed
Rob Cannon <rob.cannon@axiosengineering.com>
2014-08-22 16:23:37 -0400

    Wrapped tornado launching in an actual main(). Moved static to 'apps' dir. Handle 'index.html'. Added options for port

--- pyrest.py
+++ pyrest.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import os
+from optparse import OptionParser
 
 from rest.domain import DomainInfo, DomainProperties
 from rest.waveform import Waveforms
@@ -7,36 +8,63 @@ from rest.component import Component, ComponentProperties
 from rest.devicemanager import DeviceManagers
 from rest.device import Devices
 
+import tornado.httpserver
 import tornado.ioloop
 import tornado.web
 import tornado.websocket
 
-cwd = os.getcwd()
 
-application = tornado.web.Application([
-    (r"/static/(.*)", tornado.web.StaticFileHandler, {"path": cwd+"/static"}),
+class Application(tornado.web.Application):
+    def __init__(self):
+        cwd = os.getcwd()
 
-    (r"/rh/rest/domains/?", DomainInfo),
-    (r"/rh/rest/domains/([^/]+)/?", DomainInfo),
+        handlers = [
+            (r"/apps/(.*)/$", IndexHandler),
+            (r"/apps/(.*)", tornado.web.StaticFileHandler, {"path": cwd+"/apps"}),
 
-    (r"/rh/rest/domains/([^/]+)/properties/?", DomainProperties),
-    (r"/rh/rest/domains/([^/]+)/properties/([^/]+)", DomainProperties),
+            (r"/rh/rest/domains/?", DomainInfo),
+            (r"/rh/rest/domains/([^/]+)/?", DomainInfo),
 
-    (r"/rh/rest/domains/([^/]+)/waveforms/?", Waveforms),
-    (r"/rh/rest/domains/([^/]+)/waveforms/([^/]+)", Waveforms),
+            (r"/rh/rest/domains/([^/]+)/properties/?", DomainProperties),
+            (r"/rh/rest/domains/([^/]+)/properties/([^/]+)", DomainProperties),
 
-    (r"/rh/rest/domains/([^/]+)/waveforms/([^/]+)/components/?", Component),
-    (r"/rh/rest/domains/([^/]+)/waveforms/([^/]+)/components/([^/]+)", Component),
-    (r"/rh/rest/domains/([^/]+)/waveforms/([^/]+)/components/([^/]+)/properties", ComponentProperties),
+            (r"/rh/rest/domains/([^/]+)/waveforms/?", Waveforms),
+            (r"/rh/rest/domains/([^/]+)/waveforms/([^/]+)", Waveforms),
 
-    (r"/rh/rest/domains/([^/]+)/deviceManagers/?", DeviceManagers),
-    (r"/rh/rest/domains/([^/]+)/deviceManagers/([^/]+)", DeviceManagers),
+            (r"/rh/rest/domains/([^/]+)/waveforms/([^/]+)/components/?", Component),
+            (r"/rh/rest/domains/([^/]+)/waveforms/([^/]+)/components/([^/]+)", Component),
+            (r"/rh/rest/domains/([^/]+)/waveforms/([^/]+)/components/([^/]+)/properties", ComponentProperties),
 
-    (r"/rh/rest/domains/([^/]+)/deviceManagers/([^/]+)/devices/?", Devices),
-    (r"/rh/rest/domains/([^/]+)/deviceManagers/([^/]+)/devices/([^/]+)", Devices)
-])
+            (r"/rh/rest/domains/([^/]+)/deviceManagers/?", DeviceManagers),
+            (r"/rh/rest/domains/([^/]+)/deviceManagers/([^/]+)", DeviceManagers),
+
+            (r"/rh/rest/domains/([^/]+)/deviceManagers/([^/]+)/devices/?", Devices),
+            (r"/rh/rest/domains/([^/]+)/deviceManagers/([^/]+)/devices/([^/]+)", Devices)
+        ]
+        settings = {}
+        tornado.web.Application.__init__(self, handlers, **settings)
+
+
+class IndexHandler(tornado.web.RequestHandler):
+    def get(self, path):
+        self.render("apps/"+path+"/index.html")
+
+
+def main():
+    parser = OptionParser()
+    parser.add_option("-p", "--port", dest='port', default=8080, type='int', help="server port")
+    (options, args) = parser.parse_args()
+
+    port = options.port
+    print "Starting server on port %s" % port
+
+    application = Application()
+    http_server = tornado.httpserver.HTTPServer(application)
+
+    http_server.listen(port)
+
+    tornado.ioloop.IOLoop.instance().start()
 
 if __name__ == '__main__':
-    application.listen(8080)
-    tornado.ioloop.IOLoop.instance().start()
+    main()
 
