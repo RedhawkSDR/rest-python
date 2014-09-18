4bd02ba1f60fed56b09a6ac2647b2e8f4e7af9f1
Rob Cannon <rob.cannon@axiosengineering.com>
2014-09-18 09:41:54 -0400

    Made the BulkIO Socket support all port types. Added data conversion map and functions for Float and Double

--- rest/bulkio_handler.py
+++ rest/bulkio_handler.py
@@ -1,15 +1,11 @@
 # system imports
 import logging
 
-from bulkio.bulkioInterfaces import BULKIO, BULKIO__POA
+from bulkio.bulkioInterfaces import BULKIO__POA
 
 # third party imports
-import tornado
 from tornado import ioloop
-from tornado.httpclient import AsyncHTTPClient
-from tornado import web
 from tornado import websocket
-from tornado import gen
 
 import numpy
 
@@ -24,7 +20,21 @@ def _floats2bin(flist):
     """
     return numpy.array(flist).astype('float32').tostring()
 
+
+def _doubles2bin(flist):
+    """
+        Converts a list of python floating point values
+        to a packed array of IEEE 754 64 bit floating point
+    """
+    return numpy.array(flist).astype('float64').tostring()
+
+
 class BulkIOWebsocketHandler(websocket.WebSocketHandler):
+
+    data_conversion_map = {
+        'dataFloat':  _floats2bin,
+        'dataDouble': _doubles2bin
+    }
 
     def initialize(self, kind, _ioloop=None):
         self.kind = kind
@@ -32,18 +42,39 @@ class BulkIOWebsocketHandler(websocket.WebSocketHandler):
             _ioloop = ioloop.IOLoop.current()
         self._ioloop = _ioloop
 
-
     def open(self, *args):
         try:
             logging.debug("BulkIOWebsocketHandler open kind=%s, path=%s", self.kind, args)
             obj, path = Domain.locate_by_path(args, path_type=self.kind)
             logging.debug("Found object %s", dir(obj))
-            self.port = obj.getPort(path[0])
-            logging.debug("Found port %s", self.port)
 
-            self.async_port = AsyncPort(BULKIO__POA.dataFloat, self._pushSRI, self._pushPacket)
-            self._portname = 'myport%s' % id(self)
-            self.port.connectPort(self.async_port.getPort(), self._portname)
+            for p in obj.ports:
+                if p.name == path[0]:
+                    if p._direction == 'Uses':
+                        data_type = p._using.name
+                        namespace = p._using.nameSpace
+
+                        if namespace == 'BULKIO':
+                            self.port = obj.getPort(str(path[0]))
+                            logging.debug("Found port %s", self.port)
+
+                            self.converter = self.data_conversion_map[data_type]
+
+                            bulkio_poa = getattr(BULKIO__POA, data_type)
+                            logging.debug(bulkio_poa)
+
+                            self.async_port = AsyncPort(bulkio_poa, self._pushSRI, self._pushPacket)
+                            self._portname = 'myport%s' % id(self)
+                            self.port.connectPort(self.async_port.getPort(), self._portname)
+
+                            break
+                        else:
+                            raise ValueError("Port '%s' is not a BULKIO port" % path[0])
+                    else:
+                        raise ValueError("Port '%s' is not a uses" % path[0])
+                else:
+                    raise ValueError("Could not find port of name '%s'" % path[0])
+
         except ResourceNotFound, e:
             self.write_message(dict(error='ResourceNotFound', message=str(e)))
             self.close()
@@ -51,7 +82,6 @@ class BulkIOWebsocketHandler(websocket.WebSocketHandler):
             logging.exception('Error with request %s' % self.request.full_url())
             self.write_message(dict(error='SystemError', message=str(e)))
             self.close()
-
 
     def on_message(self, message):
         logging.debug('stream message[%d]: %s', len(message), message)
@@ -62,8 +92,6 @@ class BulkIOWebsocketHandler(websocket.WebSocketHandler):
             self.port.disconnectPort(self._portname)
         except Exception, e:
             logging.exception('Error disconnecting port %s' % self._portname)
-
-
 
     def _pushSRI(self, SRI):
         self._ioloop.add_callback(self.write_message, 
@@ -83,8 +111,7 @@ class BulkIOWebsocketHandler(websocket.WebSocketHandler):
     def _pushPacket(self, data, ts, EOS, stream_id):
 
         # FIXME: need to write ts, EOS and stream id
-        self._ioloop.add_callback(self.write_message, _floats2bin(data), binary=True)
-
+        self._ioloop.add_callback(self.write_message, self.converter(data), binary=True)
 
     def write_message(self, *args, **ioargs):
         # hide WebSocketClosedError because it's very likely
